b1ae5052e3a6b5659c9ac1c793a7dc4e5a8f8a1d
Aleksandr <trololo1009@gmail.com>
2022-06-24 01:48:43 +0500

    try to fix confirm[4]

--- js/script.js
+++ js/script.js
@@ -164,17 +164,17 @@ window.addEventListener(`DOMContentLoaded`, (e) => {
   if (document.documentElement.clientWidth < 767) {
     document.querySelectorAll("section.portfolio a img").forEach((link) => {
       link.addEventListener(`click`, (e) => {
-        // e.preventDefault();
-        // console.log(e.target.classList.contains(`not-adaptive`));
         if (e.target.classList.contains(`not-adaptive`) && e.target.classList.contains(`ru`)) {
           const mobileConfirm = confirm(`Сайт, который вы пытаетесь посетить, не оптимизирован для мобильных устройств. Нажмите ОК, чтобы перейти`);
           if (mobileConfirm == false) {
             e.preventDefault();
+            mobileConfirm = null;
           }
         } else if (e.target.classList.contains(`not-adaptive`) && e.target.classList.contains(`eng`)) {
           const mobileConfirm = confirm(`The site you are trying to visit is not mobile friendly. Click ok to go`);
           if (mobileConfirm == false) {
             e.preventDefault();
+            mobileConfirm = null;
           }
         }
       });
